86443766cc4957be0bb6b2fc301ad101eb831316
Gunhoon Lee <gunhoon@gmail.com>
2024-01-08 15:51:43 +0900

    독립 실행형 서블릿 애플리케이션 > 서블릿 등록 #2
    replace with lambda

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -16,18 +16,15 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
-			@Override
-			public void onStartup(ServletContext servletContext) throws ServletException {
-				servletContext.addServlet("hello", new HttpServlet() {
-					@Override
-					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-						resp.setStatus(200);
-						resp.setHeader("Content-Type", "text/plain");
-						resp.getWriter().println("Hello Servlet");
-					}
-				}).addMapping("/hello");
-			}
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+				}
+			}).addMapping("/hello");
 		});
 		webServer.start();
 	}
