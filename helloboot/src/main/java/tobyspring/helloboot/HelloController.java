b310329a7876d51057ca59313fcbbdc3669c8449
Gunhoon Lee <gunhoon@gmail.com>
2024-01-10 16:47:02 +0900

    독립 실행형 스프링 애플리케이션 > @Component 스캔 #4

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RequestMapping("/hello")
-@Controller
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -17,8 +17,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping
-    @ResponseBody
+    @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
